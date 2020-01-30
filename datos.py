752f705cb65dc2ec316df5d88757ee62c2777014
tamaramtz <31171329+tamaramtz@users.noreply.github.com>
2020-01-30 08:49:22 -0600

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,8 @@
+# -- ------------------------------------------------------------------------------------ -- #
+# -- Proyecto: Repaso de python 3 y analisis de precios OHLC                              -- #
+# -- Codigo: datos.py - script con datos de uso en proyecto                               -- #
+# -- Rep: https://github.com/ITESOIF/MyST/tree/master/Notas_Python/Notas_RepasoPython     -- #
+# -- Autor: Tamara Mtz                                                                  -- #
+# -- ------------------------------------------------------------------------------------ -- #
+
+OA_Ak = 'c' + '5fba46c78cbb851638af0507fd785ad-00385cb652552b6ee7215c37051eda5' + '0'
